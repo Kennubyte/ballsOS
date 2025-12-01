cab3b4cc1beee07da60741033314621d570587b0
Kennubyte <kenoleander@gmail.com>
2025-12-01 20:31:06 +0200

    started work on installe

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo Installing ballsOS
+rm -rf /var/tmp/ballsOS
+
+git clone https://github.com/Kennubyte/ballsOS.git /var/tmp/ballsOS
+cd /var/tmp/ballsOS
+
+mkdir -p /opt/ballsOS/web-ui
+cp -r management/* /opt/ballsOS/web-ui/
+
+echo Cleaning up.
+rm -rf /var/tmp/ballsOS
+echo Installation complete!
