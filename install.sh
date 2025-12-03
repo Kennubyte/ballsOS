61ee5d715e76431faa2f9e15709efbbdf6769596
Kennubyte <kenoleander@gmail.com>
2025-12-03 21:09:04 +0200

    feat: Add resource monitor service and update installation script

--- install.sh
+++ install.sh
@@ -27,21 +27,32 @@ cd /var/tmp/ballsOS
 # Prepare directories
 mkdir -p /opt/ballsOS/auth
 mkdir -p /opt/ballsOS/web-ui
+mkdir -p /opt/ballsOS/resource-monitor
 
 # Copy web-ui files
 cp -r management/* /opt/ballsOS/web-ui/
+cd /opt/ballsOS/web-ui/
+bun install
+bun run build &
 
 # Build auth provider
 cd /var/tmp/ballsOS/auth
 go build -o /opt/ballsOS/auth/ &
 
+# Build resource monitor
+cd /var/tmp/ballsOS/resource-monitor
+go build -o /opt/ballsOS/resource-monitor/ &
+
 # Set up PostgreSQL
 POSTGRES_PASS=$(openssl rand -base64 48 | tr '+/' '-_' | tr -d '\n')
 docker run --name ballsOS-postgres -p 5432:5432 -e POSTGRES_PASSWORD=$POSTGRES_PASS -e POSTGRES_DB=ballsOS -d postgres
+
+# Create .env files
 echo "POSTGRES_PASSWORD=$POSTGRES_PASS" > /opt/ballsOS/auth/.env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASS" > /opt/ballsOS/resource-monitor/.env
 
 # Move systemd service files
-mv /var/tmp/ballsOS/auth/auth-provider.service /etc/systemd/system/auth-provider.service
+mv /var/tmp/ballsOS/systemd/* /etc/systemd/system/
 
 # wait for compilation to finish
 wait
@@ -49,9 +60,11 @@ wait
 # Enable services
 systemctl daemon-reload
 systemctl enable auth-provider.service
+systemctl enable resource-monitor.service
 
 # Start services
 systemctl start auth-provider.service
+systemctl start resource-monitor.service
 
 # Clean up broski, you nasty
 echo Cleaning up.
